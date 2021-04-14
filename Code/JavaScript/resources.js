25fe79876ff28d3fb49ed540cd84f39e2826393a
Mariq <mzilcheva19@codingburgas.bg>
2021-04-14 18:56:01 +0300

    Add teams in resources and edit buttons

--- Code/JavaScript/resources.js
+++ Code/JavaScript/resources.js
@@ -1,15 +1,18 @@
 function Team1() {
-    document.getElementById("button-resources-team1").value = "Отбор 1 пътува към теб!";
+    document.getElementById("button-resources-team1").value = "Не е наличен";
+    document.getElementById("button-resources-team1").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
     document.getElementById("button-resources-team2").disabled = true;
     document.getElementById("button-resources-team3").disabled = true;
 }
 function Team2() {
-    document.getElementById("button-resources-team2").value = "Отбор 2 пътува към теб!";
+    document.getElementById("button-resources-team2").value = "Не е наличен";
+    document.getElementById("button-resources-team2").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
     document.getElementById("button-resources-team1").disabled = true;
     document.getElementById("button-resources-team3").disabled = true;
 }
 function Team3() {
-    document.getElementById("button-resources-team3").value = "Отбор 3 пътува към теб!";
+    document.getElementById("button-resources-team3").value = "Не е наличен";
+    document.getElementById("button-resources-team3").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
     document.getElementById("button-resources-team1").disabled = true;
     document.getElementById("button-resources-team2").disabled = true;
 }
