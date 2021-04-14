fbbb7ef68e94614c7eb22eec6996447edd266b3b
Mariq <mzilcheva19@codingburgas.bg>
2021-04-14 19:43:27 +0300

    Add fire trucks to resources

--- Code/JavaScript/resources.js
+++ Code/JavaScript/resources.js
@@ -16,3 +16,22 @@ function Team3() {
     document.getElementById("button-resources-team1").disabled = true;
     document.getElementById("button-resources-team2").disabled = true;
 }
+
+function Firetruck1() {
+    document.getElementById("button-resources-firetruck1").value = "Не е наличен";
+    document.getElementById("button-resources-firetruck1").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
+    document.getElementById("button-resources-firetruck2").disabled = true;
+    document.getElementById("button-resources-firetruck3").disabled = true;
+}
+function Firetruck2() {
+    document.getElementById("button-resources-firetruck2").value = "Не е наличен";
+    document.getElementById("button-resources-firetruck2").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
+    document.getElementById("button-resources-firetruck1").disabled = true;
+    document.getElementById("button-resources-firetruck3").disabled = true;
+}
+function Firetruck3() {
+    document.getElementById("button-resources-firetruck3").value = "Не е наличен";
+    document.getElementById("button-resources-firetruck3").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
+    document.getElementById("button-resources-firetruck1").disabled = true;
+    document.getElementById("button-resources-firetruck2").disabled = true;
+}
