0ebc587409ece4b6c153a9a04ab2ed6b68703f51
Mirena Dzhebarova <msdzhebarova19@codingburgas.bg>
2021-04-19 21:57:46 +0300

    Add working counter for teams and trucks

--- Code/JavaScript/resources.js
+++ Code/JavaScript/resources.js
@@ -1,39 +1,44 @@
 
-function Team1() {    
+function Team1() {
     document.getElementById("button-resources-team1").value = "Не е наличен!";
     document.getElementById("button-resources-team1").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
+    document.getElementById("h1-teams").innerHTML = "Налични отбори: 2";
     document.getElementById("button-resources-team2").disabled = true;
     document.getElementById("button-resources-team3").disabled = true;
 }
 function Team2() {
     document.getElementById("button-resources-team2").value = "Не е наличен!";
     document.getElementById("button-resources-team2").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
+    document.getElementById("h1-teams").innerHTML = "Налични отбори: 2";
     document.getElementById("button-resources-team1").disabled = true;
     document.getElementById("button-resources-team3").disabled = true;
 }
 function Team3() {
     document.getElementById("button-resources-team3").value = "Не е наличен!";
     document.getElementById("button-resources-team3").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
+    document.getElementById("h1-teams").innerHTML = "Налични отбори: 2";
     document.getElementById("button-resources-team1").disabled = true;
     document.getElementById("button-resources-team2").disabled = true;
 }
 
 function Firetruck1() {
-    
     document.getElementById("button-resources-firetruck1").value = "Не е наличен!";
     document.getElementById("button-resources-firetruck1").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
+    document.getElementById("h1-trucks").innerHTML = "Налични камиони: 2";
     document.getElementById("button-resources-firetruck2").disabled = true;
     document.getElementById("button-resources-firetruck3").disabled = true;
 }
 function Firetruck2() {
     document.getElementById("button-resources-firetruck2").value = "Не е наличен!";
     document.getElementById("button-resources-firetruck2").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
+    document.getElementById("h1-trucks").innerHTML = "Налични камиони: 2";
     document.getElementById("button-resources-firetruck1").disabled = true;
     document.getElementById("button-resources-firetruck3").disabled = true;
 }
 function Firetruck3() {
     document.getElementById("button-resources-firetruck3").value = "Не е наличен!";
     document.getElementById("button-resources-firetruck3").style.cssText = 'display: inline-block; margin-top: 2em; background: #BC1414; border-radius: 5px; line-height: 4em; padding: 0 3em; margin-left: 50px; letter-spacing: 0.25em; text-decoration: none; text-transform: uppercase; outline: 0; white-space: nowrap; font-size: 0.80em; color: rgb(224, 218, 218)';
+    document.getElementById("h1-trucks").innerHTML = "Налични камиони: 2";
     document.getElementById("button-resources-firetruck1").disabled = true;
     document.getElementById("button-resources-firetruck2").disabled = true;
 }
