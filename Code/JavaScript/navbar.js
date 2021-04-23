188ba648431350e4c184f23610e1f9dbe1a2e431
Mirena Dzhebarova <msdzhebarova19@codingburgas.bg>
2021-04-24 00:03:35 +0300

    Fix positioning of resources counters

--- Code/JavaScript/navbar.js
+++ Code/JavaScript/navbar.js
@@ -1,8 +1,8 @@
 function navbar() {
-    var x = document.getElementById("menu");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("menu");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
